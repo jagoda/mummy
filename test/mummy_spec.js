905c7f0ea3836b0c699ef3dc37f6cf9d76a91851
Jeff Jagoda <jeffrey.jagoda@gmail.com>
2014-07-29 16:12:01 -0400

    Basic pack support.
    Closes #3

--- test/mummy_spec.js
+++ test/mummy_spec.js
@@ -1,5 +1,6 @@
 "use strict";
 var Browser = require("zombie");
+var fs      = require("fs");
 var Hapi    = require("hapi");
 var nock    = require("nock");
 var Lab     = require("lab");
@@ -15,6 +16,11 @@ var expect   = Lab.expect;
 var it       = Lab.it;
 
 describe("mummy", function () {
+
+	function removeExtension () {
+		// Zombie does not expose a clean way to clear extensions.
+		Browser._extensions = _.without(Browser._extensions, mummy);
+	}
 
 	before(function (done) {
 		nock.disableNetConnect();
@@ -93,8 +99,7 @@ describe("mummy", function () {
 
 		after(function (done) {
 			embalm.restore();
-			// Zombie does not expose a clean way to clear extensions.
-			Browser._extensions = _.without(Browser._extensions, mummy);
+			removeExtension();
 			done();
 		});
 
@@ -103,6 +108,84 @@ describe("mummy", function () {
 
 			expect(embalm.callCount, "not embalmed").to.equal(1);
 			expect(embalm.calledWith(server, browser), "wrong arguments").to.be.true;
+			done();
+		});
+	});
+
+	describe("wrapping a pack of servers", function () {
+		var defaultText;
+		var dnsText;
+		var portText;
+		var anonymousText1;
+		var anonymousText2;
+
+		before(function (done) {
+			var pack    = new Hapi.Pack();
+			var browser;
+
+			pack.server("example.com");  // http://example.com:80
+			pack.server(42);             // http://localhost:42
+			pack.server();               // http://localhost:80
+			pack.server({                // https://localhost:443
+				tls : {
+					cert : fs.readFileSync(path.join(__dirname, "fixtures", "cert.pem")),
+					key  : fs.readFileSync(path.join(__dirname, "fixtures", "key.pem"))
+				}
+			});
+
+			// FIXME: depending on `_servers` is a little hackish...
+			_.each(pack._servers, function (server, index) {
+				server.route({
+					method : "GET",
+					path   : "/",
+
+					handler : function (request, reply) {
+						reply("server " + index);
+					}
+				});
+			});
+
+			Browser.extend(mummy(pack));
+			browser = new Browser();
+
+			browser.visit("/")
+			.then(function () {
+				defaultText = browser.text("body");
+				return browser.visit("http://example.com");
+			})
+			.then(function () {
+				dnsText = browser.text("body");
+				return browser.visit("http://localhost:42");
+			})
+			.then(function () {
+				portText = browser.text("body");
+				return browser.visit("http://localhost:80");
+			})
+			.then(function () {
+				anonymousText1 = browser.text("body");
+				return browser.visit("https://localhost");
+			})
+			.then(function () {
+				anonymousText2 = browser.text("body");
+			})
+			.nodeify(done);
+		});
+
+		after(function (done) {
+			removeExtension();
+			done();
+		});
+
+		it("uses the first server in the pack as the default site", function (done) {
+			expect(defaultText, "default text").to.equal("server 0");
+			done();
+		});
+
+		it("resolves servers using hostname and port", function (done) {
+			expect(dnsText, "explicit hostname").to.equal("server 0");
+			expect(portText, "explicit port").to.equal("server 1");
+			expect(anonymousText1, "anonymous http").to.equal("server 2");
+			expect(anonymousText2, "anonymous https").to.equal("server 3");
 			done();
 		});
 	});
