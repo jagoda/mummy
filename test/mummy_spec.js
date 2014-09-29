c86d3b084b144ad6399c36110f00c7db49e4af92
Jeff Jagoda <jeffrey.jagoda@gmail.com>
2014-09-29 12:26:11 -0400

    Remove unneeded object and rework test wording.
    Closes #9.

--- test/mummy_spec.js
+++ test/mummy_spec.js
@@ -467,60 +467,58 @@ describe("mummy", function () {
 		});
 	});
 
-	describe("user credentials", function () {
-		describe("when set", function () {
-			var credentials = { id : "test" };
-			var browser;
-			var server;
-			var inject;
+	describe("with user credentials", function () {
+		var credentials = { id : "test" };
+		var browser;
+		var server;
+		var inject;
+
+		before(function (done) {
+			server = new Hapi.Server();
+
+			browser = new Browser();
+			mummy.embalm(server, browser);
+
+			inject = sinon.stub(server, "inject");
+
+			browser.credentials.set(credentials);
+
+			done();
+		});
+
+		after(function (done) {
+			inject.restore();
+			done();
+		});
+
+		describe("performing a request", function () {
 
 			before(function (done) {
-				server = new Hapi.Server();
+				browser.visit("/").nodeify(done);
+			});
 
-				browser = new Browser();
-				mummy.embalm(server, browser);
+			it("puts the credentials in the request", function (done) {
+				var credentialsMatcher = sinon.match.has("credentials", credentials);
+				expect(inject.getCall(0).calledWith(credentialsMatcher, sinon.match.func)).to.be.true;
+				done();
+			});
+		});
 
-				inject = sinon.stub(server, "inject");
-
-				browser.credentials.set(credentials);
-
+		describe("that are cleared", function () {
+			before(function (done) {
+				browser.credentials.clear();
 				done();
 			});
 
-			after(function (done) {
-				inject.restore();
-				done();
-			});
-
-			describe("a request sent to Hapi", function () {
-
+			describe("performing a request", function () {
 				before(function (done) {
 					browser.visit("/").nodeify(done);
 				});
 
-				it("puts the credentials in the request", function (done) {
+				it("does not put the credentials in the request", function (done) {
 					var credentialsMatcher = sinon.match.has("credentials", credentials);
-					expect(inject.getCall(0).calledWith(credentialsMatcher, sinon.match.func)).to.be.true;
+					expect(inject.getCall(1).calledWith(credentialsMatcher, sinon.match.func)).to.be.false;
 					done();
-				});
-			});
-
-			describe("after cleared", function () {
-				before(function (done) {
-					browser.credentials.clear();
-					done();
-				});
-
-				describe("a request sent to Hapi", function () {
-					before(function (done) {
-						browser.visit("/").nodeify(done);
-					});
-
-					it("does not put the credentials in the request", function (done) {
-						var credentialsMatcher = sinon.match.has("credentials", credentials);
-						expect(inject.getCall(1).calledWith(credentialsMatcher, sinon.match.func)).to.be.false;
-						done();
-					});
 				});
 			});
 		});
